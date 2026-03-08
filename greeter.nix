b1aedb2270244993448de6553bcf6ce00a4b7164
Alex H. Raber <alexhraber@gmail.com>
2026-03-07 20:31:59 -0800

    Adjust Ghostty and greeter UX

--- greeter.nix
+++ greeter.nix
@@ -13,6 +13,7 @@
             --asterisks \
             --remember \
             --remember-user-session \
+            --theme 'text=white;time=darkgray;container=black;border=darkgray;title=white;greet=lightgray;prompt=blue;input=white;action=darkgray;button=blue' \
             --greeting "Password unlock" \
             --cmd Hyprland
         '';
